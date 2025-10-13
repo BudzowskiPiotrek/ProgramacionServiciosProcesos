29fa17504349a20c6c5ec1fa3a5be610a7d10b21
Piotrek Budzowski <piobudzows@gmail.com>
2025-10-13 19:47:39 +0200

    .

--- tema1/src/ejercicio15/Tuberia.java
+++ tema1/src/ejercicio15/Tuberia.java
@@ -1,9 +1,11 @@
 package ejercicio15;
 
 public class Tuberia {
+	
 	private int estadoCuenta = 5000;
 
 	public void actuar(int cantidad) {
+		System.out.println("entrando al cajero");
 		
 		synchronized (this) {
 			while (this.estadoCuenta <= 4000) {
