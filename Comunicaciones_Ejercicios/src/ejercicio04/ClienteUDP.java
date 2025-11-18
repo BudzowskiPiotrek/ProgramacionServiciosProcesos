b55878a3076e7e0be2128b192517990d646e3cbc
Piotrek Budzowski <piobudzows@gmail.com>
2025-11-18 21:20:32 +0100

    nuevo ejercicio

---
+++ Comunicaciones_Ejercicios/src/ejercicio04/ClienteUDP.java
@@ -0,0 +1,35 @@
+package ejercicio04;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ClienteUDP {
+
+	public static void main(String[] args) {
+		try {
+			InetAddress destino = InetAddress.getLocalHost();
+			int port = 12345; // Puerto al que envío el datagrama
+			byte[] mensaje = new byte[1024];
+
+			String Saludo = "enviado saludos !!";
+			mensaje = Saludo.getBytes(); // codifico String a bytes
+
+			// CONSTRUYO EL DATAGRAMA A ENVIAR
+			DatagramPacket envio = new DatagramPacket(mensaje, mensaje.length, destino, port);
+			DatagramSocket socket = new DatagramSocket(1234);// Puerto local
+			System.out.println("Enviando Datagrama de longitud: " + mensaje.length);
+			System.out.println("IP destino: " + destino.getHostAddress());
+			System.out.println("Host destino: " + destino.getHostName());
+			System.out.println("Puerto local del socket: " + socket.getLocalPort());
+			System.out.println("Puerto al que envio: " + envio.getPort());
+			// ENVÍO DEL DATAGRAMA
+			socket.send(envio);
+			socket.close(); // cierro el socket
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ Comunicaciones_Ejercicios/src/ejercicio04/ServidorUDP.java
@@ -0,0 +1,35 @@
+package ejercicio04;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class ServidorUDP {
+
+	public static void main(String[] args) {
+		byte[] bufer = new byte[1024]; // bufer para
+		// recibir el datagrama
+
+		try {
+			DatagramSocket socket = new DatagramSocket(12345); // Asociación del socket alpuerto 12345
+			// Espera del Datagrama
+			System.out.println("Esperando Datagrama.......");
+			DatagramPacket recibo = new DatagramPacket(bufer, bufer.length);
+			socket.receive(recibo); // Recibo del datagrama
+			int bytesRec = recibo.getLength();// Obtengo número de bytes
+			String paquete = new String(recibo.getData()); // Obtengo el String
+			// Visualización de la informaci
+			System.out.println("Numero de bytes recibidos: " + bytesRec);
+			System.out.println("Contenido del paquete: " + paquete.trim());
+			System.out.println("Puerto origen del mensaje: " + recibo.getPort());
+			System.out.println("IP de origen: " + recibo.getAddress().getHostAddress());
+			System.out.println("Puerto destino del mensaje: " + socket.getLocalPort());
+			socket.close(); // Cierre del Socket
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
